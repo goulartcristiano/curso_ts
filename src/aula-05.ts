1b065362804a71dbc9149be1ee7012bf4330ee0f
Cristiano <cristianovgoulart@gmail.com>
2023-05-29 16:16:03 -0300

    Add existing file

---
+++ src/aula-05.ts
@@ -0,0 +1,16 @@
+//Tipagem
+interface ObjetoQualquer {
+  funcao: (param1: number, param2?: number) => number;
+}
+const funcaoTeste = (param1: number, param2?: number): number => {
+  if (!param2) {
+    return param1;
+  }
+  return param1 + param2;
+};
+
+const objetoQualquer: ObjetoQualquer = {
+  funcao: funcaoTeste,
+};
+
+console.log(objetoQualquer.funcao(1));
